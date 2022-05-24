9274f815294ba37e60f430eb511857fbfc892dd9
Gabriel Santo <gvgesanto2@gmail.com>
2022-05-24 12:03:48 -0300

    feat: Add the store and remove books from localStorage functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,42 @@
 import initialBooks from './data/books.data.js';
-import genNewIdForElem from './utils/id.utils.js';
+import { genNewIdForElem, getIdsFromElemsArray } from './utils/id.utils.js';
 import {
   getInputAuthorValue,
   getInputTitleValue,
   addEventHandlerToAddForm,
 } from './views/add-book-form.view.js';
-import { addBook, removeBookFromScreen, renderBooksList } from './views/books.view.js';
+import {
+  addBook,
+  removeBookFromScreen,
+  renderBooksList,
+} from './views/books.view.js';
 
-let books = [...initialBooks];
+const storeBooksData = (books) => {
+  localStorage.setItem('books', JSON.stringify(books));
+};
+
+const getBooksData = () => {
+  const books = localStorage.getItem('books');
+  if (books) {
+    return JSON.parse(books);
+  }
+  return [];
+};
+
+const booksStored = getBooksData();
+let books = booksStored.length > 0
+  ? [
+    ...initialBooks,
+    ...booksStored.filter(
+      ({ id }) => !getIdsFromElemsArray(initialBooks).includes(id),
+    ),
+  ]
+  : initialBooks;
 
 const removeBook = (bookId) => {
   removeBookFromScreen(bookId);
   books = books.filter((book) => book.id !== bookId);
+  storeBooksData(books);
 };
 
 renderBooksList('books', books, removeBook);
@@ -26,4 +51,5 @@ addEventHandlerToAddForm((event) => {
   };
   addBook(newBook, removeBook);
   books.push(newBook);
+  storeBooksData(books);
 });
--- scripts/utils/id.utils.js
+++ scripts/utils/id.utils.js
@@ -1,8 +1,8 @@
-const genNewIdForElem = (elemsArray) => {
+export const genNewIdForElem = (elemsArray) => {
   if (elemsArray.length > 0) {
     return elemsArray[elemsArray.length - 1].id + 1;
   }
   return 1;
 };
 
-export default genNewIdForElem;
+export const getIdsFromElemsArray = (elemsArray) => elemsArray.map((elem) => elem.id);
